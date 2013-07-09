fab4fcfd2d7eb59a3ed1b404265b775109a3b236
Termitnjak d.o.o <info@termitnjak.si>
2013-07-09 09:19:39 +0000

    Changes to setup.py so it actually installs :)

--- setup.py
+++ setup.py
@@ -1,34 +1,34 @@
 from setuptools import setup, find_packages
-import os
 
 version = '0.4.dev0'
 
-setup(name='tribuna.diazotheme',
-      version=version,
-      description="Plone theme based on Twitter's Bootstrap CSS",
-      long_description=open("README.rst").read() + "\n" +
-                       open("HISTORY.txt").read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
+setup(
+    name='tribuna.diazotheme',
+    version=version,
+    description="Plone theme based on Twitter's Bootstrap CSS",
+    long_description=open("README.rst").read() + "\n" +
+                     open("HISTORY.txt").read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
         "Programming Language :: Python",
-        ],
-      keywords='plone diazo theme',
-      author='Izhar Firdaus',
-      author_email='izhar@inigo-tech.com',
-      url='https://github.com/kagesenshi/tribuna.diazotheme',
-      license='Apache License 2.0',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['diazotheme'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    ],
+    keywords='tribuna diazo theme',
+    author='',
+    author_email='',
+    url='',
+    license='Apache License 2.0',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['tribuna'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
