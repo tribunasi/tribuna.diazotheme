2177e29a525c40e9c7037800fdc551722183505a
Termitnjak d.o.o <info@termitnjak.si>
2013-07-12 17:21:56 +0000

    add a template for entry page

---
+++ tribuna/diazotheme/static/js/tribuna.js
@@ -0,0 +1,10 @@
+(function () {
+    "use strict";
+
+    jQuery17(document).ready(function () {
+        jQuery17('#entrypage-link').click(function () {
+            jQuery17('#entrypage-form-container').toggleClass('expanded');
+        });
+    });
+
+}());
