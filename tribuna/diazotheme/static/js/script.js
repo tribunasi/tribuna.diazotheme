69caeec645ebe5c93e56fe0440fc2f10e7cad243
Termitnjak d.o.o <info@termitnjak.si>
2013-07-22 18:20:08 +0000

    Monkey patch on edit form

--- tribuna/diazotheme/static/js/script.js
+++ tribuna/diazotheme/static/js/script.js
@@ -1,21 +1,39 @@
-/* Author: 
+/* Author:
 
 */
 
-$(document).ready(function () {
-    $('.field.error').each(function (idx, el) {
-        if ($.trim($(el).text()) == '') {
-            $(el).remove();
-        }
-    });
-    jQuery17('.dropdown-toggle').click(function () {
-        var self = $(this).parent();
-        $('.dropdown.open').each(function (idx, item) {
-            if ($(item)[0] != self[0]) {
-                $(item).removeClass('open');
+jQuery17(function () {
+    "use strict";
+
+    $(document).ready(function () {
+        $('.field.error').each(function (idx, el) {
+            if ($.trim($(el).text()) == '') {
+                $(el).remove();
             }
-        })
+        });
+        jQuery17('.dropdown-toggle').click(function () {
+            var self = $(this).parent();
+            $('.dropdown.open').each(function (idx, item) {
+                if ($(item)[0] != self[0]) {
+                    $(item).removeClass('open');
+                }
+            })
+        });
+
+        jQuery17('.carousel').carousel();
+
     });
 
-    jQuery17('.carousel').carousel();
+    $(window).load( function() {
+        setTimeout(function() {
+            // Crappy monkey fix that we will change ASAP
+            $('#form-widgets-IBodyText-text_parent').attr('id', 'blablakrentagtosmjst');
+            $('#blablakrentagtosmjst').attr('style', 'display: block');
+            $('#form-widgets-IBodyText-text_parent').remove();
+            $('#blablakrentagtosmjst').attr('id', 'form-widgets-IBodyText-text_parent');
+        }, 500);
+
+    })
+
+
 })
