c0b03c6645c109526ca1b9215cda9bf92efa9c52
Termitnjak d.o.o <info@termitnjak.si>
2013-08-05 16:07:06 +0000

    entrypage template and js cleanup, add close button to the entrypage form

--- tribuna/diazotheme/static/js/tribuna-entrypage.js
+++ tribuna/diazotheme/static/js/tribuna-entrypage.js
@@ -1,14 +1,20 @@
 jQuery17(function () {
+    "use strict";
+
     jQuery17(document).ready(function () {
         jQuery17('#entrypage-link').click(function () {
-                    jQuery17('#entrypage-form-container').toggleClass('expanded');
-                });
-        console.info(jQuery17("#entrypage-text"));
+            jQuery17('#entrypage-form-container').toggleClass('expanded');
+            jQuery17('#entrypage-form-close').toggle();
+        });
+        jQuery17('#entrypage-form-close').click(function () {
+            jQuery17('#entrypage-form-container').toggleClass('expanded');
+            jQuery17('#entrypage-form-close').toggle();
+        });
         $("#entrypage-text").fitText();
         $($(".accordion-toggle")[0]).css("text-decoration", "underline");
-        $($(".accordion-toggle")[0]).attr("clicked", "clicked")
+        $($(".accordion-toggle")[0]).attr("clicked", "clicked");
         $(".accordion-toggle").click(function() {
-            if($(this).attr("clicked") == "clicked") {
+            if($(this).attr("clicked") === "clicked") {
                 $(this).css("text-decoration", "");
                 $(this).attr("clicked", "");
             }
@@ -18,9 +24,8 @@ jQuery17(function () {
                 $(".accordion-toggle").not(this).each(function(){
                     $(this).css("text-decoration", "");
                     $(this).attr("clicked", "");
-
-                    });
-                }
+                });
+            }
         });
     });
 });
