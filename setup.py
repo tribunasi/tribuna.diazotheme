9576b5d700bdca02de56edc8df9628ae14697ee7
Natan Žabkar <natan.zabkar@gmail.com>
2013-12-31 18:45:38 +0100

    Start 0.2dev

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import setup, find_packages
 
 import os
 
-version = '0.1'
+version = '0.2dev'
+
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
